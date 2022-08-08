b86825251cbd64b1c6785b0d7d3cd1c940f8596f
eunyoung <shineeol20@gmail.com>
2022-08-08 14:34:40 +0900

    fix: test 수정

--- my-agora-states-client/src/__test__/App.test.js
+++ my-agora-states-client/src/__test__/App.test.js
@@ -3,6 +3,6 @@ import App from '../App';
 
 it('My Agora States', () => {
     render(<App />);
-    const h1Element = screen.getByText(/test1/i);
+    const h1Element = screen.getByText(/My Agora States/i);
     expect(h1Element).toBeInTheDocument();
 })
