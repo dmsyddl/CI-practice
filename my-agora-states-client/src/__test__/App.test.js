fe2d377870f77c609a9aeab03029c9a0bcf0713d
eunyoung <shineeol20@gmail.com>
2022-08-08 15:21:34 +0900

    feat: simple test 추가

--- my-agora-states-client/src/__test__/App.test.js
+++ my-agora-states-client/src/__test__/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-it("CI practice", () => {
-  render(<App />);
-  const h1Element = screen.getByText(/My Agora States/i);
-  expect(h1Element).toBeInTheDocument();
+describe("simple test", () => {
+  it("2 + 2 = 4", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it("2 - 1 = 1", () => {
+    expect(2 - 1).toBe(1);
+  });
 });
