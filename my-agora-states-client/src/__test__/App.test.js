5c45123dd4762df481f1474e4a4f3547eb7f9795
eunyoung <shineeol20@gmail.com>
2022-08-08 15:37:01 +0900

    feat: test 추가

--- my-agora-states-client/src/__test__/App.test.js
+++ my-agora-states-client/src/__test__/App.test.js
@@ -9,4 +9,9 @@ describe("simple test", () => {
   it("2 - 1 = 1", () => {
     expect(2 - 1).toBe(1);
   });
+  // test를 실패하는 코드가 있다면 실패
+  // vercel은 그냥 배포된다.
+  //   it("7 + 6 = 13", () => {
+  //     expect(7 + 6).toBe(10);
+  //   })
 });
