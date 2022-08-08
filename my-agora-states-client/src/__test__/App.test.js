daff0d3238bb44898182f1f441d19304d81d1ef5
eunyoung <shineeol20@gmail.com>
2022-08-08 15:09:08 +0900

    feat: yml test 추가

--- my-agora-states-client/src/__test__/App.test.js
+++ my-agora-states-client/src/__test__/App.test.js
@@ -1,8 +1,8 @@
-import {render,screen} from '@testing-library/react';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-it('My Agora States', () => {
-    render(<App />);
-    const h1Element = screen.getByText(/My Agora States/i);
-    expect(h1Element).toBeInTheDocument();
-})
+it("CI practice", () => {
+  render(<App />);
+  const h1Element = screen.getByText(/My Agora States/i);
+  expect(h1Element).toBeInTheDocument();
+});
